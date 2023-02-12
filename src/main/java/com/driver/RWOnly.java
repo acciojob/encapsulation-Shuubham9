c6ea50be7a3a941a726300a271d694132731356c
shuubham <vaggysv@gmail.com>
2023-02-12 18:50:44 +0530

    third assignment

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name;
+    public void setName(int name){
+        this.name=name;
+    }
+    public int getName(){
+        return name;
+    }
 }
